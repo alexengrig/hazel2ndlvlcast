d7cf9e852391f89681b6eecb35a3e23a9f9b7baf
Grig Alex <alexengrigdev@gmail.com>
2022-07-18 00:00:48 +0300

    Add Hazelcast as 2nd level cache
    Thing(id=1, name=test)

--- src/main/java/dev/alexengrig/sample/hazel2ndlvlcast/entity/Thing.java
+++ src/main/java/dev/alexengrig/sample/hazel2ndlvlcast/entity/Thing.java
@@ -6,12 +6,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @Getter
 @Setter
 @Builder
